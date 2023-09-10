338aecd8cbddf635b635740bec73b6325d40fb3c
Henrique toledo <123999555+henriqueferreirasantos@users.noreply.github.com>
2023-09-10 18:45:08 -0300

    ex12pronto

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,18 @@
-alert('olá')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas !`
+    if (hora >= 0 && hora < 12) {
+        img.src ='pexels-andre-moura-2768483.jpg' 
+        document.body.style.background = 'yellow'
+    }else if (hora >= 12 && hora <= 18){
+        img.src ='pexels-tom-fisk-18158930.jpg'
+        document.body.style.background = 'orange'
+    }else{
+        img.src ='pexels-james-wheeler-414612.jpg'
+        document.body.style.background = 'black'
+    }
+}
